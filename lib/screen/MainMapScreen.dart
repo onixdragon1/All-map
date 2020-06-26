5cbccffd057226b18aef7a70ffb6b7eb720780f6
ShinJuwon <41102652+onixdragon1@users.noreply.github.com>
2020-06-26 11:50:57 +0900

    Refactoring MainMapScreen.dart

--- lib/screen/MainMapScreen.dart
+++ lib/screen/MainMapScreen.dart
@@ -27,164 +27,23 @@ class _MainMapScreenState extends State<MainMapScreen> {
       child: Scaffold(
         body: Center(
           child: Container(
-            width: width * 1,
-            height: height * 1,
+            // width: width * 1,
+            // height: height * 1,
             child: Column(
               children: <Widget>[
-                
                 Stack(
                   children: <Widget>[
-                    /*Google Map이 들어올 자리*/
+                    /* Background에 들어갈 Google Map 설정 시작 */
                     /*Google Map API 받아서 백그라운드로 맵 설정 후 Floating Menu 구현*/
-                    Container(
-                      decoration: BoxDecoration(color: Colors.redAccent),
-                    ),
-                    
-                    Container(
-                      alignment: Alignment.center,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.all(width * 0.018),
-                          ),
-                          Container(
-                            width: width * 0.8,
-                            child:TextField(
-                              obscureText: true,
-                              controller: mySearch_Controller,
-                              decoration: InputDecoration(
-                                isDense: true,
-                                prefixIcon: new Icon(Icons.menu),
-                                border: OutlineInputBorder(),
-                                hintText: '장소, 상호명 및 주소 검색',
-                                hintStyle: TextStyle(
-                                  fontSize: 12,
-                                ),
-                                fillColor: Color.fromRGBO(228, 255, 223, 1),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    showGoogleMap(width, height),
+                    /* Background에 들어갈 Google Map 설정 끝 */
+
+                    /* 상단 Search 영역 설정 시작*/
+                    showSearchInput(width, height, mySearch_Controller),
+                    /* 상단 Search 영역 설정 끝 */
+
                     /* 하단 NAVIGATION BAR 설정 시작*/
-                    Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(top: width * 1.433),  // 1.4125 | 1.433 수치값 개발시 알아서 조정
-                      height: height * 0.1,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(14),
-                          topLeft: Radius.circular(14),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromRGBO(0, 0, 0, 0.5),
-                            blurRadius: 2.0,
-                            spreadRadius: 1.0,
-                            offset: Offset(0.0, -2.0), // shadow direction: bottom right
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          Stack(
-                            alignment: Alignment.center,
-                            children: <Widget>[
-                              Image.asset(
-                                'Images/upload.png',
-                                fit: BoxFit.cover,
-                                width: width * 0.1,
-                                height: width * 0.1,
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  /* 길 찾기 메뉴 클릭 시 이동할 페이지로의 이동 코드 부분 */
-                                },
-                                child: Text(
-                                  '길 찾기',
-                                  style: TextStyle(
-                                    fontSize: width * 0.03,
-                                    backgroundColor: Color.fromRGBO(255, 255, 255, 0.7),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Stack(
-                            alignment: Alignment.center,
-                            children: <Widget>[
-                              Image.asset(
-                                'Images/shop.png',
-                                fit: BoxFit.cover,
-                                width: width * 0.1,
-                                height: width * 0.1,
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  /* 주변 상점 메뉴 클릭 시 이동할 페이지로의 이동 코드 부분 */
-                                },
-                                child: Text(
-                                  '주변 상점',
-                                  style: TextStyle(
-                                    fontSize: width * 0.03,
-                                    backgroundColor: Color.fromRGBO(255, 255, 255, 0.7),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Stack(
-                            alignment: Alignment.center,
-                            children: <Widget>[
-                              Image.asset(
-                                'Images/shop_package.png',
-                                fit: BoxFit.cover,
-                                width: width * 0.1,
-                                height: width * 0.1,
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  /* 포인트 교환 메뉴 클릭 시 이동할 페이지로의 이동 코드 부분 */
-                                },
-                                child: Text(
-                                  '포인트 교환',
-                                  style: TextStyle(
-                                    fontSize: width * 0.03,
-                                    backgroundColor: Color.fromRGBO(255, 255, 255, 0.7),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Stack(
-                            alignment: Alignment.center,
-                            children: <Widget>[
-                              Image.asset(
-                                'Images/settings.png',
-                                fit: BoxFit.cover,
-                                width: width * 0.1,
-                                height: width * 0.1,
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  /* 설정 메뉴 클릭 시 이동할 페이지로의 이동 코드 부분 */
-                                },
-                                child: Text(
-                                  '설정',
-                                  style: TextStyle(
-                                    fontSize: width * 0.03,
-                                    backgroundColor: Color.fromRGBO(255, 255, 255, 0.7),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                    showBottomNavbar(width, height),
                     /* 하단 NAVIGATION BAR 설정 끝 */
                   ],
                 ),
@@ -192,6 +51,185 @@ class _MainMapScreenState extends State<MainMapScreen> {
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget showGoogleMap(width, height){
+    return Container(
+      width: width * 1,
+      height: height * 0.9645, //0.9645
+      decoration: BoxDecoration(color: Colors.lightBlueAccent),
+    );
+  }
+
+  Widget showSearchInput(width, height, mySearch_Controller){
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(width * 0.018),
+          ),
+          Container(
+            width: width * 0.8,
+            child:TextField(
+              obscureText: true,
+              controller: mySearch_Controller,
+              decoration: InputDecoration(
+                filled: true,
+                isDense: true,
+                prefixIcon: new Icon(Icons.menu),
+                border: OutlineInputBorder(),
+                hintText: '장소, 상호명 및 주소 검색',
+                hintStyle: TextStyle(
+                  fontSize: 12,
+                ),
+                fillColor: Color.fromRGBO(228, 255, 223, 1),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget makeBottomNavbarComponent(name, width){
+    
+    if(name == "길 찾기"){
+      return Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Image.asset(
+            'Images/upload.png',
+            fit: BoxFit.cover,
+            width: width * 0.09,
+            height: width * 0.09,
+          ),
+          InkWell(
+            onTap: () {
+              /* 길 찾기 메뉴 클릭 시 이동할 페이지로의 이동 코드 부분 */
+            },
+            child: Text(
+              '길 찾기',
+              style: TextStyle(
+                fontSize: width * 0.0275,
+                backgroundColor: Color.fromRGBO(255, 255, 255, 0.85),
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    if(name == "주변 상점"){
+      return Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Image.asset(
+            'Images/shop.png',
+            fit: BoxFit.cover,
+            width: width * 0.09,
+            height: width * 0.09,
+          ),
+          InkWell(
+            onTap: () {
+              /* 주변 상점 메뉴 클릭 시 이동할 페이지로의 이동 코드 부분 */
+            },
+            child: Text(
+              '주변 상점',
+              style: TextStyle(
+                fontSize: width * 0.0275,
+                backgroundColor: Color.fromRGBO(255, 255, 255, 0.85),
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    if(name == "포인트 교환"){
+      return Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Image.asset(
+            'Images/shop_package.png',
+            fit: BoxFit.cover,
+            width: width * 0.09,
+            height: width * 0.09,
+          ),
+          InkWell(
+            onTap: () {
+              /* 포인트 교환 메뉴 클릭 시 이동할 페이지로의 이동 코드 부분 */
+            },
+            child: Text(
+              '포인트 교환',
+              style: TextStyle(
+                fontSize: width * 0.0275,
+                backgroundColor: Color.fromRGBO(255, 255, 255, 0.85),
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    if(name == "설정"){
+      return Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Image.asset(
+            'Images/settings.png',
+            fit: BoxFit.cover,
+            width: width * 0.09,
+            height: width * 0.09,
+          ),
+          InkWell(
+            onTap: () {
+              /* 설정 메뉴 클릭 시 이동할 페이지로의 이동 코드 부분 */
+            },
+            child: Text(
+              '설정',
+              style: TextStyle(
+                fontSize: width * 0.0275,
+                backgroundColor: Color.fromRGBO(255, 255, 255, 0.85),
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+    else return Text("존재하지 않은 컴포넌트입니다!");
+  }
+
+  Widget showBottomNavbar(width, height){
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(top: width * 1.435575),  // 1.4125 | 1.435575 수치값 개발시 알아서 조정
+      height: height * 0.1,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(14),
+          topLeft: Radius.circular(14),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.5),
+            blurRadius: 2.0,
+            spreadRadius: 1.0,
+            offset: Offset(0.0, -2.0), // shadow direction: bottom right
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          makeBottomNavbarComponent("길 찾기", width),
+          makeBottomNavbarComponent("주변 상점", width),
+          makeBottomNavbarComponent("포인트 교환", width),
+          makeBottomNavbarComponent("설정", width),
+        ],
       ),
     );
   }
